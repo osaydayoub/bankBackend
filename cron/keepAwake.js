7dc16b13b3c7bbbb6f400292426eb92a5877c034
osaydayoub <osaydayoub@gmail.com>
2024-03-24 11:36:06 +0200

    change to 14 min

--- cron/keepAwake.js
+++ cron/keepAwake.js
@@ -3,6 +3,6 @@ import cron from "node-cron";
 // Maybe make the schedule time a variable and save last update time somewhere to make sure if the server restarts it doesnt affect the schedule
 
 // Schedule the task to run every week
-cron.schedule("*/1 * * * *", () => {
+cron.schedule("*/14 * * * *", () => {
   console.log("Hello!");
 });
