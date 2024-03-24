6ca89f9b01e5a7da9b4f504e1bc46ce7bafba2ee
osaydayoub <osaydayoub@gmail.com>
2024-03-24 12:26:52 +0200

    change to 14 min

--- cron/keepAwake.js
+++ cron/keepAwake.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Maybe make the schedule time a variable and save last update time somewhere to make sure if the server restarts it doesnt affect the schedule
 
 // Schedule the task to run every week
-cron.schedule("*/1 * * * *", () => {
+cron.schedule("*/14 * * * *", () => {
   const wakeUp = async () => {
     try {
       const res = await axios.get("http://localhost:3000/test");
